dc8bc529579f6356f1ee4d251c7d13fef75c63da
Orobosa <95650588+orobosaik@users.noreply.github.com>
2022-05-13 08:38:21 +0100

    Add basic maths functions and variables

---
+++ script.js
@@ -0,0 +1,37 @@
+//Basic math function
+function add(a, b) {
+	return a + b;
+}
+function subtract(a, b) {
+	return a - b;
+}
+function multiply(a, b) {
+	return a * b;
+}
+function divide(a, b) {
+	return a / b;
+}
+function operate(operator, num1, num2) {
+	return operator(num1, num2);
+}
+function negative() {
+	Math.sign(currentNum) === 1 ?
+		currentNum = -Math.abs(currentNum) : currentNum = Math.abs(currentNum);
+}
+// Variables
+let display1 = document.querySelector(".display--first");
+let display2 = document.querySelector(".display--second");
+let prevNum = "";
+let currentNum = "";
+let operator = "";
+let answer = "";
+let display = "";
+
+theObject = {
+	"*": multiply,
+	"+": add,
+	"-": subtract,
+	"/": divide,
+	"=": operate,
+};
+let signOp = Object.keys(theObject);
